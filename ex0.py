d9d2dc04104733a1242363ff9fd95dea1eb95712
shimischwartz <shimon.schwartz@mail.ac.il>
2021-11-03 18:00:21 +0200

    first commit

---
+++ ex0.py
@@ -0,0 +1,8 @@
+#########################################################
+#FILE:ex0.py
+#WRITER:shimon_schwartz , shimioschwartz , 201572807
+#EXERCIS:intro2cs1 ex0 2018-2019
+#DESRIPTION: A simple program that prints "Hello World!" to
+#the standard output (screen).
+#########################################################
+print("Hello World!")
